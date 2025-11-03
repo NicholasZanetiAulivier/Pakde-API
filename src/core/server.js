8d61a10a6feb566376020993d54731d4e894cd20
NicholasZanetiAulivier <auliviernicholaszaneti@gmail.com>
2025-11-03 20:23:50 +0700

    Added basic error logging

--- src/core/server.js
+++ src/core/server.js
@@ -1,7 +1,24 @@
 const express = require('express');
 
 const config = require('./config');
+const logger = require('./logger');
 
 const app = express();
+
+app.use(logger);
+
+app.get('/', (req, res) => {
+    res.send(`${dwidwi}`);
+})
+
+//Error Response
+app.use((err, req, res, next) => {
+    res.status(err.status || 500).json({
+        statusCode: err.status || 500,
+        error: err.code || 'UNKNOWN_ERROR',
+        description: err.description || 'Unknown error',
+        message: err.message || 'An error has occurred',
+    });
+});
 
 module.exports = app;
--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
+const { env, port } = require('./core/config');
 const server = require("./core/server");
 
-const app = server.listen();
+const app = server.listen(port, (err) => {
+    if (err) {
+        // logger.fatal(err, 'Failed to stat the server.');
+        process.exit(1);
+    } else {
+        // logger.info(`Server runs at port ${port} in ${env} environment`);
+    }
+});
+
+process.on('uncaughtException', (err) => {
+    // logger.fatal(err, 'Uncaught exception.');
+
+    app.close(() => process.exit(1));
+
+    setTimeout(() => process.abort(), 1000).unref();
+    process.exit(1);
+});
