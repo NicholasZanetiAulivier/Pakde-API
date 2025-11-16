2a571440fd77ebbdfa5b4247d3297de8c6ac75b4
NicholasZanetiAulivier <auliviernicholaszaneti@gmail.com>
2025-11-16 18:52:46 +0700

    fix

--- src/core/config.js
+++ src/core/config.js
@@ -3,10 +3,10 @@ const fs = require('fs');
 
 process.env.NODE_ENV = (process.env.NODE_ENV || 'development').toLowerCase();
 
-const envFound = dotenv.config({ path: '.env' });
-if (envFound.error) {
-    throw new Error("⚠️ Couldn't find .env file ⚠️");
-}
+// const envFound = dotenv.config({ path: '.env' });
+// if (envFound.error) {
+//     throw new Error("⚠️ Couldn't find .env file ⚠️");
+// }
 
 module.exports = {
     env: process.env.NODE_ENV,
