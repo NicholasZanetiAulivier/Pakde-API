a74f8eeba2b86e185479198502d843c67bfba0e6
NicholasZanetiAulivier <auliviernicholaszaneti@gmail.com>
2025-11-16 22:53:27 +0700

    render

--- api/index.js
+++ api/index.js
@@ -4,13 +4,13 @@ const server = require("./core/server");
 const app = express();
 server(app);
 
-process.on('uncaughtException', (err) => {
-    // logger.fatal(err, 'Uncaught exception.');
+// process.on('uncaughtException', (err) => {
+//     // logger.fatal(err, 'Uncaught exception.');
 
-    // db.end();
-    app.close(() => process.exit(1));
+//     // db.end();
+//     app.close(() => process.exit(1));
 
-    setTimeout(() => process.abort(), 1000).unref();
-    process.exit(1);
-});
+//     setTimeout(() => process.abort(), 1000).unref();
+//     process.exit(1);
+// });
 module.exports = app;
