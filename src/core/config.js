db696081c04baa50918e96092a834e5ae495bc46
NicholasZanetiAulivier <auliviernicholaszaneti@gmail.com>
2025-11-17 08:59:03 +0700

    Delete .env dependency

--- src/core/config.js
+++ src/core/config.js
@@ -1,8 +1,8 @@
 const fs = require('fs');
 
 process.env.NODE_ENV = (process.env.NODE_ENV || 'development').toLowerCase();
-const { loadEnvFile } = require('node:process');
-loadEnvFile('.env');
+// const { loadEnvFile } = require('node:process');
+// loadEnvFile('.env');
 module.exports = {
     env: process.env.NODE_ENV,
     api: {
